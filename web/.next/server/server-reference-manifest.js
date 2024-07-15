e32b7b5d1d59c73ae7f841c8eff00dc92aa272f2
hieunmh <hieunm.pt@gmail.com>
2024-07-15 23:21:34 +0700

    delete

---
+++ web/.next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"eIj0qj6SSQ43TWkTeCmyZJ97MJyz8IlFOX4ViFXaz2U=\"\n}"
--- web/lib/apiEndPoints.ts
+++
@@ -1,5 +0,0 @@
-export const API_URL = process.env.NEXT_PUBLIC_API_URL + "/api";
-export const SIGNIN_URL = "/auth/signin";
-export const SIGNUP_URL = "/auth/signup";
-export const CHECK_CREDENTIALS = "/auth/checkCredentials";
-export const SIGNOUT_URL = "/auth/signout";
